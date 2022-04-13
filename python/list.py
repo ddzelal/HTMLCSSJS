44e1f30fed57efc434705731028c4f1340b58fcc
Dzelal Dupljak <56730336+ddzelal@users.noreply.github.com>
2022-04-13 03:37:46 +0200

    drugizdpy

--- python/list.py
+++ python/list.py
@@ -1,3 +1,6 @@
+from keyword import kwlist
+
+
 lista = ["Dzelal","Dupljak",23,"Novi Pazar"]
 print (lista)
 print(lista[0])
@@ -6,3 +9,14 @@ print(lista[2])
 print(lista[3])
 
 print(lista[0]+" "+lista[1])
+
+klub = ["Novi Pazar"]
+klub.append("CZV")
+klub.append("Partizan")
+
+
+klub.insert(0,23)
+klub.insert(2,"Zeljo")
+klub = klub + [33,"Partizan",1999,"Barca"]
+print(klub)
+print(len(klub))
