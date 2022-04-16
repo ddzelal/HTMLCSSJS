512a36658b9c2bbe9d75d002dc9dd127e185f7a3
Dzelal Dupljak <56730336+ddzelal@users.noreply.github.com>
2022-04-16 03:17:16 +0200

    whilezad

---
+++ python/While.py
@@ -0,0 +1,14 @@
+
+
+brojcanik = 1
+while brojcanik<6:
+    print("pet puta nesto  se ponavlja")
+
+    brojcanik = brojcanik +1
+
+print("Dosta bre!")
+
+br = 4
+while br<=20:
+    print(br)
+    br = br +1
