1a87dcb6e4fdeb9de6a53d19fc3467e030a6f763
Dzelal Dupljak <56730336+ddzelal@users.noreply.github.com>
2022-04-25 03:07:12 +0200

    funkkraj

--- python/funkc2.py
+++ python/funkc2.py
@@ -6,3 +6,19 @@ def kvadrat (a):
 
 stranica = float(input("Unesi stranicu"))
 kvadrat(stranica)
+
+def pravogaonik(a,b):
+    povrsina= a*b
+    obim = 2*a+2*b
+
+    print (f" o = {obim} p = {povrsina}")
+
+st_a = float(input("Unesi str a"))
+st_b = float(input("Unesi str b"))
+pravogaonik(st_a,st_b)
+
+stranice = []
+stranice.append(st_a = float(input("Unesi str a")))
+stranice.append(st_a = float(input("Unesi str b")))
+
+obim_pr=pravogaonik (stranice[0],stranice[1])
