a8cfdd97cf1292951d2e2f6294052c5fc2a3bf95
Dzelal Dupljak <56730336+ddzelal@users.noreply.github.com>
2022-04-29 06:14:17 +0200

    ZadatakResen

--- JavaScriptG9/prvicas.js
+++ JavaScriptG9/prvicas.js
@@ -1,9 +1,45 @@
 godine = prompt("Unesite vase godine");
 starost  = 2022 - godine;
-
+if (godine % 400 === 0){
+    console.log("Vasa godina rodjenja je prestupna");
+}
+else if(godine % 100!=0 && godine%4===0){
+    console.log("Vasa godina rodjenja je prestupna");
+}
+else{
+    console.log("Vasa godina rodjenja nije prestupna");
+}
 console.log("Vi ste rodjeni",godine,"godine i imate",starost, "godine");
 
-mesec = prompt ("Unesite mesec rodjenja i dan rodjenja");
+const var1 = prompt ("Unesite mesec rodjenja i dan rodjenja");
+
+ var2 = var1.split(".");
+
+ meseci = [
+    [1,"januar"],
+    [2,"februar"],
+    [3,"mart"],
+     [4,"april"],
+      [5,"maj"],
+       [6,"jun"],
+       [7,"jul"], 
+       [8,"avgust"], 
+       [9,"septembar"], 
+       [10,"oktobmar"], 
+       [11,"novembar"], 
+       [12,"decemabarar"]
+]
 
 
-const mesec = text.split(".");
+// array1.forEach(element => console.log(element));
+// NE ZNAM
+
+
+
+console.log(meseci);
+ 
+ 
+
+ console.log(var2);
+
+ console.log ("Datum vaseg rodjenja je",var2[0],"mesec","i dan",var2[1]);
