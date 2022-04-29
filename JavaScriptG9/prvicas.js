6f1639a23034947e1d74969a8da4e0dacf499eb5
Dzelal Dupljak <56730336+ddzelal@users.noreply.github.com>
2022-04-29 03:46:04 +0200

    domacijs

--- JavaScriptG9/prvicas.js
+++ JavaScriptG9/prvicas.js
@@ -1,1 +1,9 @@
-console.log("Hello world")
+godine = prompt("Unesite vase godine");
+starost  = 2022 - godine;
+
+console.log("Vi ste rodjeni",godine,"godine i imate",starost, "godine");
+
+mesec = prompt ("Unesite mesec rodjenja i dan rodjenja");
+
+
+const mesec = text.split(".");
