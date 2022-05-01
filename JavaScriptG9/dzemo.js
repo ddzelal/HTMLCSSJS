457bb7455b956ad8c9a2d90c26aade39a45ed953
Dzelal <dzelal_dlive.comgit config --global user.name Dzelal#git config --global user.email dzelal_dlive.com>
2022-05-01 02:23:20 +0200

    resen

---
+++ JavaScriptG9/dzemo.js
@@ -0,0 +1,45 @@
+unos = prompt("Unesite dva elemnta u formatu x.y")
+
+// console.log(unos_parse);
+
+unos_s = unos.split(".");
+
+prvi = parseInt(unos_s[0]);
+drugi = parseInt(unos_s[1]);
+
+
+console.log(prvi,drugi);
+
+rezultat = prvi + drugi;
+
+console.log("rezultat dva elemnta:",rezultat);
+
+
+
+var twoSum = function(nums, target) {
+    
+};
+
+target = 9;
+
+nums = [2,7,11,15];
+// ova petlja ide kroz ceo niz od pocetka do kraja
+res = []
+for (i = 0; i < nums.length; i++)
+{  
+    for (let j = i+1; j < nums.length; j++) {
+        if (nums[i] + nums[j] === target) {
+            res.push(i)
+            res.push(j)
+        } 
+    }   
+}
+
+console.log(res)
+
+
+
+
+
+
+
