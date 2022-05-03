eaa94fa6f0e33b804d30ca474e20b2af051163f0
Dzelal Dupljak <56730336+ddzelal@users.noreply.github.com>
2022-05-03 11:27:10 +0200

    csv

--- JavaScriptG9/dzemo.js
+++ JavaScriptG9/dzemo.js
@@ -25,7 +25,8 @@ target = 9;
 nums = [2,7,11,15];
 // ova petlja ide kroz ceo niz od pocetka do kraja
 res = []
-for (i = 0; i < nums.length; i++)
+//prazan niz
+for (i = 0; i < nums.length; i++) 
 {  
     for (let j = i+1; j < nums.length; j++) {
         if (nums[i] + nums[j] === target) {
