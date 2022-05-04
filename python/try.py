189988e2a8248e2c924eb76e5e5a1b1e945660c9
Dzelal Dupljak <56730336+ddzelal@users.noreply.github.com>
2022-05-04 19:25:49 +0200

    try

---
+++ python/try.py
@@ -0,0 +1,11 @@
+# try and ex
+
+
+try:
+    broj = int(input("Unesited ceo broj"))
+    rezultat = 2/broj
+    print(f"Rezultat {rezultat} ")
+except:
+    print("Pojavila se greska0") 
+finally:
+    print("Ovaj text sed uvek izvrsava")
