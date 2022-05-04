e3817daf79019b62446f9470c29f3cd63ff1dbe6
Dzelal Dupljak <56730336+ddzelal@users.noreply.github.com>
2022-05-04 19:31:58 +0200

    finshtry

--- python/try.py
+++ python/try.py
@@ -5,7 +5,11 @@ try:
     broj = int(input("Unesited ceo broj"))
     rezultat = 2/broj
     print(f"Rezultat {rezultat} ")
+except ZeroDivisionError:
+    print("Uneti broj nije deljiv") 
+except ValueError:
+    print("Broj se ne deli sa textom") 
 except:
-    print("Pojavila se greska0") 
+    print("Pojavila se greska")
 finally:
     print("Ovaj text sed uvek izvrsava")
