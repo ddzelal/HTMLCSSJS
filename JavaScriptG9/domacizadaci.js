0430e055e31b7228dae78ad2bb62b7d50386f3d8
Dzelal <dzelal_dlive.comgit config --global user.name Dzelal#git config --global user.email dzelal_dlive.com>
2022-05-05 22:35:33 +0200

    zadacijs

---
+++ JavaScriptG9/domacizadaci.js
@@ -0,0 +1,50 @@
+
+
+for(i=1; i <=3; i++){
+
+console.log("python")
+
+    for(j=1; j<=3; j++){
+
+    console.log("je super")
+
+    }
+
+}
+
+broj = 20;   //ispisi sve parne brojeve 
+
+for (i=1; i<=broj; i++)
+{
+    if(i%2===0){
+        console.log(i)
+    }
+
+
+}
+
+
+ispis = 10;  // ispis svih brojeva do unetog broja
+for(i=1; i<=ispis; i++){
+    console.log(i)
+}
+
+
+proizvoljanbroj = 21;
+parni=0;
+neparani=0;
+
+for(i=1; i<=proizvoljanbroj; i++){
+
+    if(i%2===0){
+        parni = parni +i;
+        console.log(i,"proizvod je paran i zbir mu je",parni);
+    }
+    else if (i%2===1)
+    {
+        neparani = neparani +1;
+        console.log(i,"proizvod je neparan i zbir mu je ",neparani);
+    }
+
+
+}
---
+++ JavaScriptG9/vezjbe.js
@@ -0,0 +1,25 @@
+
+for (i = 1; i<=10; i++){
+    console.log("Ispisi nesto",i)
+}
+
+
+for (i=1; i<=10; i++){
+    if(i !== 7){
+        console.log(i)
+    }
+
+}
+suma = 0;
+for (i = 1; i<=20; i++){
+    if(i % 2 === 0){  
+       
+        suma = suma +i;
+        console.log(i,suma)
+       
+    }
+    
+    
+    
+    
+}
