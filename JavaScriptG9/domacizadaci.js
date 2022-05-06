1f449dd9a9b93ea46f6e64e3e6141d06eed44f73
Dzelal <dzelal_dlive.comgit config --global user.name Dzelal#git config --global user.email dzelal_dlive.com>
2022-05-06 15:06:32 +0200

    finishzadaci

--- JavaScriptG9/domacizadaci.js
+++ JavaScriptG9/domacizadaci.js
@@ -1,36 +1,48 @@
 
 
-for(i=1; i <=3; i++){
 
-console.log("python")
 
-    for(j=1; j<=3; j++){
 
-    console.log("je super")
+switch(meni = prompt("koji zadatak zelis")){
+    case 'x': 
 
-    }
+        for(i=1; i <=3; i++){
 
-}
+        console.log("python")
 
-broj = 20;   //ispisi sve parne brojeve 
+        for(j=1; j<=3; j++){
 
-for (i=1; i<=broj; i++)
-{
-    if(i%2===0){
+        console.log("je super")
+
+        }
+        
+
+            }
+    break;
+     case 'y':
+        broj = 20;   //ispisi sve parne brojeve 
+
+        for (i=1; i<=broj; i++)
+        {
+            if(i%2===0){
+                console.log(i)
+            }
+        
+        
+        }
+    break;        
+
+    case 'z':
+
+        ispis = 10;  // ispis svih brojeva do unetog broja
+        for(i=1; i<=ispis; i++){
         console.log(i)
-    }
+        }
 
+    break;
 
-}
-
-
-ispis = 10;  // ispis svih brojeva do unetog broja
-for(i=1; i<=ispis; i++){
-    console.log(i)
-}
-
-
-parni_br = []
+    case 'w':
+        parni_br = []
 neparni_br = []
 proizvoljanbroj = 21;
 parni=0;
@@ -54,9 +66,21 @@ for(i=1; i<=proizvoljanbroj; i++){
 }
 
     console.log("parni brojevi su",parni_br,"Neparni brojevi su:",neparni_br);
+    break;
+ case 'p':
 
 nazad = 8;
 
 for(i=8; i>=1; i--){
     console.log(i);
 }
+
+    
+    break;
+
+}
+
+
+
+
+
