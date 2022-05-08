70306c0f9986b6f7d7fd59224db682f2023a2582
ddzelal <56730336+ddzelal@users.noreply.github.com>
2022-05-08 16:15:32 +0200

    domdva

--- JavaScriptG9/dvazadaci.js
+++ JavaScriptG9/dvazadaci.js
@@ -8,21 +8,45 @@ switch(unos){
         stranica_a=Number(prompt("Unesi stranicu a"))
         stranica_b=Number(prompt("Unesi stranicu b"))
         stranica_c=Number(prompt("Unesi stranicu c"))
-        a = stranica_a + stranica_b;
-        b = stranica_c + stranica_a;
-        c =
+        // a = stranica_a + stranica_b;
+        // b = stranica_c + stranica_a;
+        // c =
 
         console.log("Uneli ste straniu a:",stranica_a,"stranicu b:",stranica_b,"stranicu c:",stranica_c)
 
+        if (stranica_a === stranica_b & stranica_a === stranica_c & stranica_b === stranica_c){
+                console.log("Uneli ste jednakostranicni toougao")
+        }
+        else if(stranica_a !== stranica_b & stranica_a !== stranica_c & stranica_b !== stranica_c){
+                console.log("Uneli ste raznostranicni trougao")
+        }
+        else{
+            console.log("Uneli ste jednakokraki trougao")
         
+        }
+           
+        break;
+
+    case 2:
+
+     br_ucenika = Number(prompt("Koliko zelis ucenika"))
+     visina = [];
+
+     for(i=0; i<br_ucenika; i++){
+         v = Number(prompt("visina ucenika?"))
+        visina.push(v)
         
+         
+     }
+     console.log(visina)
 
-
-
+     minv = Math.min.apply(Math,visina)
+     console.log("Najnizi ucenik je visok:",minv,"CM")
+       
+  
 
 
         break;
-
 
 
 
