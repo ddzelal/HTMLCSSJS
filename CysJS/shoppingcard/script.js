e07e9d432804a50725530a40f845f6711fc61f62
Dzelal <dzelal_dlive.comgit config --global user.name Dzelal#git config --global user.email dzelal_dlive.com>
2022-05-19 19:42:45 +0200

    sc

--- CysJS/shoppingcard/script.js
+++ CysJS/shoppingcard/script.js
@@ -1,6 +1,42 @@
+
+let allTotal = 0;
 function addToCart(element){
     let mainEl = element.closest('.single-item')
-    let price = mainEl.querySelector('.price')
-    console.log(price)
+    let price = mainEl.querySelector('.price').innerText;
+    let name = mainEl.querySelector('h3').innerText;
+    let quantity = mainEl.querySelector('input').value;
+
+    let cartItems = document.querySelector('.cart-items');
+    
+    if(parseInt(quantity)  >0){
+
+        
+        price = price.substring(1);
+        price = parseInt(price);
+
+        let total = price * parseInt(quantity);
+        allTotal += total;
+
+        cartItems.innerHTML += `<div class="cart-single-item">
+        <h3>${name}</h3>
+        <p>$${price} x ${quantity} = $<span>${total}</span></p>
+        <button onclick="removeFromCart(this) class="remove-item">Ukloni</button>
+        
+        </div>`
+
+        document.querySelector('.total').innerText = `Total: $${allTotal}`
+        element.innerText= 'Dodato';
+        element.setAttribute('disabled','true');
+
+    }else{
+        alert("Odaberi kolicinu")
+    }
+
+    function removeFromCart(elemnt){
+        let mainEl = element.closest('.cart-single-item');
+        let price = mainEl.querySelector('.p span')
+    };
+
+    
 
 }
