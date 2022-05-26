8f3a84b58f64f5b6badb76597f7b2606a7e9b942
ddzelal <56730336+ddzelal@users.noreply.github.com>
2022-05-26 16:06:26 +0200

    zav

--- JavaScriptG9/casdva/script.js
+++ JavaScriptG9/casdva/script.js
@@ -1,18 +1,49 @@
 
 
 
+// prvi zadatak
+
+// broj = '2326212'
+// Nbroj = ''
 
 
-broj = '2324532'
-Nbroj = ''
+// for(i=0; i<broj.length; i++){
+//     if(broj[i] % 2 === 0){
+//         Nbroj +=  '-'
+//     }
+//     Nbroj+= broj[i]
+// }
+
+// console.log(Nbroj)
 
 
-for(i=0; i<broj.length; i++){
-    if(broj[i] % 2 === 0){
-        Nbroj = "-" + broj[i];
-    }
-    Nbroj+= broj[i]
+// drugizadatak
+
+// niz = [2,3,4,2,6,2,1]
+// niz2= []
+
+
+// for(i=0; i<niz.length; i++){
     
+//     for(j=i; j<niz.length; j++){
+        
+//     }
+//     if(niz[i] !== niz[j]){
+//         niz2.push(niz[i]) 
+//     }
+// }
+
+// console.log(niz2)
+
+
+niz = [2,3,12,5,-2,-32,23]
+sum = 0;
+
+for(i=0; i<niz.length; i++){
+    if(niz[i]>0){
+        sum+=niz[i]
+    }
+
 }
 
-console.log(Nbroj)
+console.log(sum)
