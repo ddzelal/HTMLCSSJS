e7161a82ebdcbaceaa75871e8686db25169a6169
ddzelal <56730336+ddzelal@users.noreply.github.com>
2022-06-01 14:16:50 +0200

    prebacaj

---
+++ JavaScriptG9/metodeNiza/script.js
@@ -0,0 +1,18 @@
+
+
+
+//Metoda concat(); spaja nizove
+
+
+niz1 = [1,2,3,4,5]
+niz2 = [2,4,5,72,23]
+nizSpajanja = niz1.concat(niz2);
+
+console.log(nizSpajanja)
+
+
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+const f = fruits.entries();
+
+console.log(f)
