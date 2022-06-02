2acffcda8edad6769071c94cc2cbac7ed3abcdd3
ddzelal <56730336+ddzelal@users.noreply.github.com>
2022-06-02 23:37:05 +0200

    kuca

--- JavaScriptG9/metodeNiza/script.js
+++ JavaScriptG9/metodeNiza/script.js
@@ -11,8 +11,25 @@ nizSpajanja = niz1.concat(niz2);
 console.log(nizSpajanja)
 
 
+//metoda find () trazi odgovarajuci element sa postavljenim uslovom! i ispisuje samo prvi nadjeni element
 
 const fruits = ["Banana", "Orange", "Apple", "Mango"];
-const f = fruits.entries();
 
-console.log(f)
+trazi = fruits.find(el => el === 'Apple');
+
+console.log(trazi)
+
+ //metoda filter() filtrira sve elemente koji ispunjavaju uslov
+
+nizFilter = nizSpajanja.filter(el => el > 5)
+console.log(nizFilter)
+
+//Ovako proveravamo da li imamo duplih elemenata
+
+const numbers = [1, 2, 3, 2, 4, 5, 5, 6];
+
+const duplicates = numbers.filter((item, index) => index !== numbers.indexOf(item));
+
+console.log(duplicates);
+
+//tst
