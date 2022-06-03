bc7dab5b41ca55d247750c4b6d72d99b1d1fbefd
ddzelal <56730336+ddzelal@users.noreply.github.com>
2022-06-03 19:04:28 +0200

    try

--- JavaScriptG9/obnavljanje/script.js
+++ JavaScriptG9/obnavljanje/script.js
@@ -2,19 +2,21 @@
 
 niz1=[[1,2,3],[4,5,6],[7,8,9]]
 str = ""
-suma = 0;
+
 
 for (i=0; i<niz1.length; i++){
+    suma = 0;
+    red = " ";
     for(j=0; j < niz1[i].length; j++){
-
-        
-        
+        suma+=niz1[i][j]
+        red += niz1[i][j]
        
+        
     }
-    
-    
-    
-   
+    // niz1.push(suma);
+    console.log(suma)
+    console.log(red)
 }
+
 
 
