8f694d4659b1515b511aa7d6565176cddf9bf9f2
ddzelal <56730336+ddzelal@users.noreply.github.com>
2022-06-05 15:01:11 +0200

    zadaci

---
+++ JavaScriptG9/domaci/script.js
@@ -0,0 +1,64 @@
+
+
+
+
+broj = [1,2,3,2,1]
+jednak = ""
+konacno=false;
+
+for (i=0; i<broj.length; i++){
+    // console.log(broj[i])
+    jednak+=broj[i]
+    if(broj[0] === broj[4] && broj[1] === broj[3]){
+    
+        konacno = true;
+
+    }
+    
+}
+
+
+if(konacno){
+    console.log("Broj je jednak:",jednak)
+}
+else{
+    console.log("Broj nije jednak")
+}
+
+function prover (i)
+{
+    if(broj[0] === broj[4] && broj[1] === broj[3]){
+    
+        konacno = true;
+
+    }
+
+}
+
+broj.forEach(element => {
+    if(prover){
+        return konacno = true;
+    }
+});
+
+console.log(broj)
+    
+//Drugi zadatak
+
+
+var longestCommonPrefix = function(strs) {
+    let prefix = strs.reduce((acc, str) => str.length < acc.length ? str : acc);
+    
+    for (let str of strs) {
+        while (str.slice(0, prefix.length) != prefix) {
+            prefix = prefix.slice(0, -1);
+        }
+    }
+    return prefix;
+};
+
+let res = longestCommonPrefix(["flower","flow","flight"]);
+
+console.log(res);
+
+
