3efbe471ad4a9f3ac8cd54caf0747af5b82ba653
Dzelal <dzelal_dlive.comgit config --global user.name Dzelal#git config --global user.email dzelal_dlive.com>
2022-06-08 20:52:18 +0200

    cas

---
+++ JavaScriptG9/objektiDomaci/script.js
@@ -0,0 +1,35 @@
+var Biblioteka = [
+    {
+        autor : 'Dzelal',
+        naslov: 'BrzZivot',
+        procitano : true
+    },
+    {  
+        autor : 'Daris',
+        naslov: 'SveZnam',
+        procitano : true
+
+    },
+    {
+        autor : 'Dzemil',
+        naslov: 'Proso sam sve',
+        procitano : true
+    }
+]
+
+
+
+for(i=0;  i<Biblioteka.length; i++){
+    console.log("Autor:",Biblioteka[i].autor,"je napisao knjigu:",Biblioteka[i].naslov)
+}
+
+
+Covek = {
+    ime: 'Dzelal',
+    prezime: 'Dupljak',
+    status: true,
+    jbmg: '123231231232'
+}
+
+
+// console.log(Covek.ime + Covek.prezime)
