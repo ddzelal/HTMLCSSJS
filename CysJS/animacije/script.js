3e847aa16a9eccf8f6c32a82f8eab75707fa9aad
Dzelal <dzelal_dlive.comgit config --global user.name Dzelal#git config --global user.email dzelal_dlive.com>
2022-06-13 22:11:48 +0200

    tst

--- CysJS/animacije/script.js
+++ CysJS/animacije/script.js
@@ -1,5 +1,41 @@
 let textTag = document.querySelector('.section1 h1')
-// let text = textTag.textContent;
+let text = textTag.textContent;
 console.log(textTag)
 
 textTag.className = "fadeMove"
+let splittedText = text.split('');
+textTag.innerHTML = '';
+for (let i = 0; i < splittedText.length; i++){
+    if(splittedText[i]=== " "){
+        splittedText[i]= "&nbsp"
+    }
+    textTag.innerHTML+= `<span>${splittedText[i]}</span>`
+}
+let spans = textTag.querySelectorAll('span')
+let k = 0;
+let interval =
+setInterval(()=>{
+    
+    let singleSpan = spans[k]
+    singleSpan.className = "fadeMove"
+
+    if(k === spans.length){
+        clearInterval(interval)
+    }
+    k++
+},70)
+
+let animationWidth = 0;
+let border = document.querySelector('.border-line')
+window.onscroll = () => {
+    if(this.oldScroll >= this.scrollY)
+    if(animationWidth >= 100){
+        animationWidth = 100;
+    }else {
+        animationWidth += 1;
+    }
+   console.log(this.scrollY)
+   border.style.width= animationWidth + '%'
+
+   this.oldScroll = this.scrollY;
+}
