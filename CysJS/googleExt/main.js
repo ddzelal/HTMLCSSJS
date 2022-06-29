85fbc9757f6947de7c253b28681554d85cbcb070
“Dzelal” <dzelal_d@live.com>
2022-06-29 18:24:31 +0200

    google

---
+++ CysJS/googleExt/main.js
@@ -0,0 +1,5 @@
+console.log("Dzki Kralju moj <3");
+
+let logo = document.querySelector('.lnXdpd')
+logo.src = chrome.runtime.getURL('cysecor_logo.png')
+logo.srcset = chrome.runtime.getURL('cysecor_logo.png')
